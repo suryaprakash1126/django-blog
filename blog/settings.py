617e61fb8a2620b6d52c28ef5fc3a92556ea22cf
suryaprakash1126 <nsuryaprakash31@gmail.com>
2025-06-26 15:05:53 +0530

    Add media file support for Render

--- blog/settings.py
+++ blog/settings.py
@@ -144,6 +144,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+RENDER = os.environ.get('RENDER')
+if RENDER:
+    MEDIA_ROOT = '/media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
